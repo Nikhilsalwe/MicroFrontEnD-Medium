36d48c52fde1783fb73685ccad50efb32285bd92
Nikhilsalwe <salwenikhil0724@gmail.com>
2022-01-28 01:03:19 +0530

    added console

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,7 +4,7 @@ import { StylesProvider } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-
+console.log('hi')
 export default () => {
   return (
     <div>
