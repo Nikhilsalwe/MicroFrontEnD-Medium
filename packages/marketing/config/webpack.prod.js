a00edbce543195246fee413b71cc40791edd88b6
Nikhilsalwe <salwenikhil0724@gmail.com>
2022-01-28 01:11:14 +0530

    marketing webpack prod corrected

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,25 +1,24 @@
-const {merge} = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const packageJson = require('../package.json')
-const commonConfig = require('./webpack.common')
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
-    name: 'production',
-    output: {
-        filename: '[name].[contenthash].js',
-        publicPath: '/marketing/latest/'
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'marketing',
-            filename: 'remoteEntry',
-            exposes: {
-                './MarketingApp': './src/bootstrap',
-            },
-            shared: packageJson.dependencies
-        })
-    ]
-}
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
-module.exports = merge(commonConfig, prodConfig)
-
+module.exports = merge(commonConfig, prodConfig);
